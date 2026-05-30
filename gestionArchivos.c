e053a736b8f3bf3193dd3e1a9a69084efc63efe2
Ignacio de Castro <ignaciodecastro36@gmail.com>
2026-05-30 01:07:37 -0300

    Update gestionArchivos.c

--- gestionArchivos.c
+++ gestionArchivos.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include "cinefiliaHeader.h"
-=======
-#include "gestionArchivos.h"
->>>>>>> a98f1e4cfaae65484fd44e121ef105cbc94cf33c
 
 t_fecha parsear_fecha(const char *cad)
 {
@@ -12,11 +8,6 @@ t_fecha parsear_fecha(const char *cad)
         return f;
 
     sscanf(cad, "%d/%d/%d", &f.d, &f.m, &f.a);
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> a98f1e4cfaae65484fd44e121ef105cbc94cf33c
     return f;
 }
 
@@ -92,16 +83,11 @@ void leerArchivo(FILE *archivo, t_vector *vecMiembros, t_fecha fechaProceso)
                 puts("error de memoria\n");
                 return;
             }
-<<<<<<< HEAD
-=======
-
->>>>>>> a98f1e4cfaae65484fd44e121ef105cbc94cf33c
         }
 
     }
 }
 
-<<<<<<< HEAD
 void mostrarArchivoMiembros(const char *pathArch)
 {
     FILE *pf = fopen(pathArch, "rb");
@@ -135,8 +121,6 @@ void mostrarArchivoMiembros(const char *pathArch)
     fclose(pf);
 }
 
-=======
->>>>>>> a98f1e4cfaae65484fd44e121ef105cbc94cf33c
 int procesarMiembro(char *registro, t_miembros *miembro, t_fecha fechaProceso)
 {
     trozado(registro, miembro);
